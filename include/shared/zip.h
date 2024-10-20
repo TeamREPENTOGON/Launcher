9769c2804551475c4e38090d9270e8fc8cc08e56
Sylmir <sylmir.dev@gmail.com>
2024-10-20 19:09:44 +0200

    Zip::ExtractFile with FILE*

--- include/shared/zip.h
+++ include/shared/zip.h
@@ -1,13 +1,18 @@
 #pragma once
 
+#include <cstdio>
 #include <zip.h>
 
 namespace Zip {
 	enum ExtractFileResult {
 		EXTRACT_FILE_OK,
 		EXTRACT_FILE_ERR_FOPEN,
-		EXTRACT_FILE_ERR_ZIP_FREAD
+		EXTRACT_FILE_ERR_ZIP_FREAD,
+		EXTRACT_FILE_ERR_ZIP_STAT
 	};
 
-	ExtractFileResult ExtractFile(zip_t* zip, int index, zip_file_t* file, const char* name);
+	ExtractFileResult ExtractFile(zip_t* zip, int index, zip_file_t* file, 
+		const char* name, const char* mode);
+	ExtractFileResult ExtractFile(zip_t* zip, int index, zip_file_t* file,
+		FILE* output);
 }
--- shared/zip.cpp
+++ shared/zip.cpp
@@ -2,24 +2,28 @@
 #include <zip.h>
 
 namespace Zip {
-	ExtractFileResult ExtractFile(zip_t* zip, int index, zip_file_t* file, const char* name) {
-		FILE* f = fopen(name, "wb");
+	ExtractFileResult ExtractFile(zip_t* zip, int index, zip_file_t* file, const char* name, const char* mode) {
+		FILE* f = fopen(name, mode);
 		if (!f) {
 			return EXTRACT_FILE_ERR_FOPEN;
 		}
 
+		ExtractFileResult result = ExtractFile(zip, index, file, f);
+		fclose(f);
+		return result;
+	}
+
+	ExtractFileResult ExtractFile(zip_t* zip, int index, zip_file_t* file, FILE* output) {
 		char buffer[4096];
 		zip_int64_t read = 0;
 		while ((read = zip_fread(file, buffer, 4096)) > 0) {
-			fwrite(buffer, read, 1, f);
+			fwrite(buffer, read, 1, output);
 		}
 
 		if (read == -1) {
-			fclose(f);
 			return EXTRACT_FILE_ERR_ZIP_FREAD;
 		}
 
-		fclose(f);
 		return EXTRACT_FILE_OK;
 	}
 }
