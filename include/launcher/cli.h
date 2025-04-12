4b3d2f599985aa482b965c8f84ef37ac7fe01545
Sylmir <sylmir.dev@gmail.com>
2025-04-12 15:50:08 +0200

    Add a few more commands to the launcher
      --repentogon-installer-wait
        > Causes the installer to wait for the user to close it by hand,
          instead of closing by itself
    
      --repentogon-installer-refresh
        > Configure the time in milliseconds between consecutive updates of
          the repentogon installer (slows down the speed at which download
          notifications are reported)
    
      --skip-self-update
        > Skip the self update check at the start of the launcher

--- include/launcher/cli.h
+++ include/launcher/cli.h
@@ -14,12 +14,31 @@ public:
         return _forceRepentogonUpdate;
     }
 
+    inline bool RepentogonInstallerWait() const {
+        return _repentogonInstallerWaitOnFinished;
+    }
+
+    inline unsigned long RepentogonInstallerRefreshRate() const {
+        return _repentogonInstallerRefreshRate;
+    }
+
+    inline bool SkipSelfUpdate() const {
+        return _skipSelfUpdate;
+    }
+
 private:
     bool _forceWizard = false;
     bool _forceRepentogonUpdate = false;
+    bool _repentogonInstallerWaitOnFinished = false;
+    bool _skipSelfUpdate = false;
+    unsigned long _repentogonInstallerRefreshRate = _repentogonInstallerDefaultRefreshRate;
 
+    static constexpr unsigned long _repentogonInstallerDefaultRefreshRate = 100;
     static constexpr const char* forceWizard = "force-wizard";
     static constexpr const char* forceRepentogonUpdate = "force-repentogon-update";
+    static constexpr const char* repentogonInstallerWait = "repentogon-installer-wait";
+    static constexpr const char* repentogonInstallerRefresh = "repentogon-installer-refresh";
+    static constexpr const char* skipSelfUpdate = "skip-self-update";
     /* static constexpr const char* help = "help";
     static constexpr const char* helpShort = "h"; */
 };
--- src/cli.cpp
+++ src/cli.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 
 #include "launcher/cli.h"
+#include "shared/logger.h"
 #include "wx/cmdline.h"
 
 int CLIParser::Parse(int argc, wxChar** argv) {
@@ -9,6 +10,11 @@ int CLIParser::Parse(int argc, wxChar** argv) {
     // parser.AddSwitch(helpShort, help, "Display this help", wxCMD_LINE_OPTION_HELP);
     parser.AddLongSwitch(forceWizard, "Force the wizard to run, even if it should not");
     parser.AddLongSwitch(forceRepentogonUpdate, "Force a Repentogon update at startup");
+    parser.AddLongSwitch(repentogonInstallerWait, "Prevent the Repentogon installer from closing after "
+        "installation is complete");
+    parser.AddLongOption(repentogonInstallerRefresh, "Time (in milliseconds) between two updates of the "
+        "installer window", wxCMD_LINE_VAL_NUMBER);
+    parser.AddLongSwitch(skipSelfUpdate, "Skip the self update checks at startup");
     int result = parser.Parse();
 
     if (result > 0)
@@ -20,6 +26,23 @@ int CLIParser::Parse(int argc, wxChar** argv) {
 
     if (parser.Found(forceRepentogonUpdate)) {
         _forceRepentogonUpdate = true;
+    }
+
+    if (parser.Found(repentogonInstallerWait)) {
+        _repentogonInstallerWaitOnFinished = true;
+    }
+
+    long refreshRate = 0;
+    if (parser.Found(repentogonInstallerRefresh, &refreshRate)) {
+        if (refreshRate <= 0) {
+            refreshRate = _repentogonInstallerDefaultRefreshRate;
+        }
+
+        _repentogonInstallerRefreshRate = refreshRate;
+    }
+
+    if (parser.Found(skipSelfUpdate)) {
+        _skipSelfUpdate = true;
     }
 
     return result;
