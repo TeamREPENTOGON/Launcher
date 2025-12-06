e2a5ec48a327d92b09fdb383163e59aefafa2899
Sylmir <sylmir.dev@gmail.com>
2025-12-06 12:53:34 +0100

    Strip wxWidgets

--- cmake/deps/wxWidgets.cmake
+++ cmake/deps/wxWidgets.cmake
@@ -1,5 +1,2 @@
 target_compile_options (wxbase PUBLIC "/MD")
 target_compile_options (wxcore PUBLIC "/MD")
-target_compile_options (wxstc PUBLIC "/MD")
-target_compile_options (wxscintilla PUBLIC "/MD")
-target_compile_options (wxlexilla PUBLIC "/MD")
